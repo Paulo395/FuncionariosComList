e7dcd6f80dfb3a9bbef62aaef44f4c209c034c9c
Paulo Oliveira <paulinho395@gmail.com>
2023-05-01 21:58:21 -0300

    Projeto Base

---
+++ Models/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AumentoUsandoList.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        //Método de cadastro que retorna a si mesmo com os dados cadastrados
+        public Employee RegisterEmpoloyee()
+        {
+            Console.Write("Id: ");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Name: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Salary: ");
+            Salary = double.Parse(Console.ReadLine());
+
+            Employee employee = new Employee(Id,Name,Salary);
+
+            return employee;
+        }
+
+        public void IncreseSalary(int porc)
+        {
+            double increse = (porc / 100) * Salary;
+            Salary += increse;
+        }
+
+        public override string ToString()
+        {
+            return "\nId: " + Id + "\nName: " + Name + "\nSalary: " + Salary + "\n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using AumentoUsandoList.Models;
+using System;
+using System.Xml.Linq;
+
+namespace AumentoUsandoList // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de uma lista de Employeers e uma instancia da mesma, para utilizar seu métodos
+            List<Employee> employees = new List<Employee>();
+            Employee e = new Employee();
+
+            //Cadastro irá receber um retorno do tipo Employee e irá ser adicionado na Lista
+            //Employee cadastro = e.RegisterEmpoloyee();
+            //employees.Add(cadastro);
+
+            //Adição regular a Lista
+            employees.Add(new Employee { Id = 0, Name = "Paulo", Salary = 700 });
+            employees.Add(new Employee { Id = 3, Name = "Henrique", Salary = 500 });
+
+            Regist(employees);
+
+            //int index = int.Parse(Console.ReadLine());
+            int index = 0;
+
+            //employees[index].Salary += e.IncreseSalary(30, employees[index].Salary);
+            employees[index].IncreseSalary(30);
+            Console.WriteLine("The salary was increse!");
+            Regist(employees);
+        }
+
+        public static void Regist(List<Employee> employees)
+        {
+            Console.Write("\nEmployeers Regist:");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
